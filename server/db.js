be4a7af04e44bfa4f037562d813bdfc5d2de8f7f
Bryan Gabriel <bryangabrielberja25@gmail.com>
2023-11-23 19:18:57 +0800

    put_the_connections_Value_in_ENV

--- server/db.js
+++ server/db.js
@@ -8,34 +8,3 @@ export const db = mysql.createConnection({
     port:process.env.DBPORT
 })
 
-
-// const config = {
-//     host: "localhost",
-//     user: "root",
-//     password: "4amyami12345",
-//     port: 3306,
-//     database: "blog"
-// };
-
-// const db = mysql.createPool(config);
-
-// module.exports = (query) => {
-//    return  new Promise((resolve, reject) => {
-//         db.getConnection((err, sql) => {
-//             if(err){
-//                 reject(err)
-//                 console.log("db error:", err)
-//             }else{
-//                 sql.query(query,(err, results) =>{
-//                     if(err){
-//                         reject(err)
-//                         console.log('sql error:', err)
-//                     }else{
-//                         resolve(results)
-//                     }
-//                     sql.release()
-//                 })
-//             }
-//         })
-//     })
-// }
