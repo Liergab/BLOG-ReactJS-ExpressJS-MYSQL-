865bf9aa8c2a79258576c3bde83a200543f04671
Bryan Gabriel <bryangabrielberja25@gmail.com>
2023-12-04 14:47:41 +0800

    fix the images and other minor things

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,27 +3,45 @@ import blog from '../assets/images/blog.jpg';
 import { Link } from 'react-router-dom';
 import { Button } from '@material-tailwind/react';
 import { useGetAllPost } from '../api/post';
+import { useState } from 'react';
+
 
 const Home = () => {
+  const [showFullText, setShowFullText] = useState(false);
+  const toggleText = () => {
+    setShowFullText(!showFullText);
+  };
+  const limitText = (text, limit) => {
+    const words = text.split(' ');
+    const limitedWords = words.slice(0, limit);
+    return limitedWords.join(' ');
+  };
+
   const {data:posts, isPending } =  useGetAllPost()
 
   if(isPending) return <h1>Loading..</h1>
   return (
     <main >
-      <div className='flex flex-col mt-14 gap-32'>
+      <div className='flex flex-col mt-32 gap-32'>
         {posts.map((post) => (
-          <div key={post?.id} className='flex gap-14 odd:flex-row-reverse '>
-            <div className='flex-[2]  post_box-showdow '>
-              <img src={post.img || blog} alt=""  className='w-full max-h-[400px] object-cover'/>
-             
+          <div key={post?.id} className='space-y-8 md:space-y-0 md:flex gap-14 odd:flex-row-reverse '>
+            <div className='md:flex-[2]  '>
+             {post?.image &&
+              (<img src={`http://localhost:3001/images/${post?.image}` || blog} alt="" 
+                className='w-full max-h-[250px] md:max-h-[400px] lg:max-h-[400px] object-cover rounded-lg'/>)} 
             </div> 
-            <div className='flex-[3] flex flex-col justify-between '>
+            <div className='flex-[3] md:flex flex-col justify-between  gap-10 space-y-6 md:space-y-0'>
               <Link to={`/post/${post.id}`}>
-                <h2 className='text-5xl font-bold'>{post?.title}</h2>
+                <h2 className='text-2xl md:text-5xl font-bold'>{post?.title}</h2>
               </Link>
-              <h1 className='text-lg font-normal'>{post?.desc}</h1>
-              <h1>{post?.userPost?.id}</h1>
-              <Button className='bg-secondary w-max'>Read More...</Button>
+              <h1 className='text-base md:text-lg font-normal text-justify'>
+              {showFullText ? post?.desc : limitText(post?.desc, 100)}
+              </h1>
+              {post?.desc && post.desc.split(' ').length > 100 && (
+              <Button className='bg-secondary w-max' onClick={toggleText}> 
+                 {showFullText ? 'Read Less' : 'Read More'}
+              </Button>
+              )}
             </div>
           </div>
         ))}
