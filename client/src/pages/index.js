f1dcb7fecf317c2309af9994d1ee39d4b7fbd4c2
Bryan Gabriel <bryangabrielberja25@gmail.com>
2023-12-04 14:52:37 +0800

    add updated post in index.js

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -3,4 +3,5 @@ export {default as Login } from './Login'
 export {default as Write } from './Write'
 export {default as Register } from './Register'
 export {default as SinglePost } from './SinglePost'
+export {default as UpdatePost } from './UpdatePost'
 
